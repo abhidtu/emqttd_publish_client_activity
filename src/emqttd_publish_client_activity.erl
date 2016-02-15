8fe90a3c8f1b289a95a5d9b9b1cd20e5d13dcf0f
abhishek chawla <abhidtu@gmail.com>
2016-02-15 23:44:59 +0530

    added epoch_timestamp and node name in json

--- src/emqttd_publish_client_activity.erl
+++ src/emqttd_publish_client_activity.erl
@@ -71,10 +71,12 @@ on_client_connected(ConnAck, _Client = #mqtt_client{client_id = ClientId}, _Env)
 
     %% build json to send using ClientId
     Json = mochijson2:encode([
-      {client_id, ClientId}
+      {client_id, ClientId},
+      {cluster_node, node()},
+      {epoch_timestamp, now_to_seconds(now())}
     ]),
 
-  publishMessageStates(ClientId,"SYSTEM/presence/connected",list_to_binary(Json)).
+  publishMessageStates(ClientId,lists:concat(["SYSTEM/",node(),"/presence/connected"]),list_to_binary(Json)).
 
 %%-----------client connect end-------------------------------------%%
 
@@ -88,10 +90,12 @@ on_client_disconnected(Reason, ClientId, _Env) ->
       %% build json to send using ClientId
       Json = mochijson2:encode([
         {client_id, ClientId},
-        {reason, Reason}
+        {reason, Reason},
+        {cluster_node, node()},
+        {epoch_timestamp, now_to_seconds(now())}
       ]),
 
-  publishMessageStates(ClientId,"SYSTEM/presence/disconnected",list_to_binary(Json)).
+  publishMessageStates(ClientId,lists:concat(["SYSTEM/",node(),"/presence/disconnected"]),list_to_binary(Json)).
 
 %%-----------client disconnect end-----------------------------------%%
 
@@ -110,10 +114,12 @@ on_client_subscribe_after(ClientId, TopicTable, _Env) ->
   %% build json to send using ClientId
   Json = mochijson2:encode([
     {client_id, ClientId},
-    {topic, TopicTable}
+    {topic, TopicTable},
+    {cluster_node, node()},
+    {epoch_timestamp, now_to_seconds(now())}
   ]),
 
-  publishMessageStates(ClientId,"SYSTEM/subscription/subscribed",list_to_binary(Json)).
+  publishMessageStates(ClientId,lists:concat(["SYSTEM/",node(),"/subscription/subscribed"]),list_to_binary(Json)).
 
 %%-----------client subscribed end----------------------------------------%%
 
@@ -127,10 +133,12 @@ on_client_unsubscribe(ClientId, Topics, _Env) ->
     %% build json to send using ClientId
     Json = mochijson2:encode([
       {client_id, ClientId},
-      {topic, Topics}
+      {topic, Topics},
+      {cluster_node, node()},
+      {epoch_timestamp, now_to_seconds(now())}
     ]),
 
-    publishMessageStates(ClientId,"SYSTEM/subscription/unsubscribed",list_to_binary(Json)),
+    publishMessageStates(ClientId,lists:concat(["SYSTEM/",node(),"/subscription/unsubscribed"]),list_to_binary(Json)),
     Topics.
 
 %%-----------client unsubscribed end----------------------------------------%%
@@ -158,10 +166,12 @@ publishMsgArrival(#mqtt_message{msgid = MsgId, pktid = PktId, from = From,
     {pub_client_id, From},
     {topic, Topic},
     {payload, Payload},
-    {qos, QoS}
+    {qos, QoS},
+    {cluster_node, node()},
+    {epoch_timestamp, now_to_seconds(now())}
   ]),
 
-  publishMessageStates(From,"SYSTEM/message",list_to_binary(Json)).
+  publishMessageStates(From,lists:concat(["SYSTEM/",node(),"/message"]),list_to_binary(Json)).
 
 %%-----------message publish end----------------------------------------%%
 
@@ -189,12 +199,14 @@ publishMsgAck(ClientId, #mqtt_message{msgid = MsgId, pktid = PktId, from = From,
       {ack_client_id, ClientId},
       {product_id, ProductId},
       {message_id, MessageId},
-      {topic,Topic}
+      {topic,Topic},
+      {cluster_node, node()},
+      {epoch_timestamp, now_to_seconds(now())}
     ]),
 
     if ProductId =/= undefined, MessageId =/= undefined ->
 
-      publishMessageStates(From,"SYSTEM/msgack",list_to_binary(Json));
+      publishMessageStates(From,lists:concat(["SYSTEM/",node(),"/msgack"]),list_to_binary(Json));
 
       true ->
       io:format(" product id or message id not defined ~n")
@@ -220,6 +232,11 @@ publishMessageStates(From,Topic,Payload)->
     true ->
       io:format("publishMsgArrival from broker ~n")
   end.
+
+%%-------------erlang time to Epoch time converter -----------------------------------%%
+  now_to_seconds({Mega, Sec, _}) ->
+    (Mega * 1000000) + Sec
+  .
 
 %%----------- publish method end ----------------------------------------%%
 
